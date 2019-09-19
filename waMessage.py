22e1fe111b75f0f7eccb6f24b9b9fc5be13895c4
markovicv <vukasinnauksa@gmail.com>
2019-09-19 13:54:25 +0200

    sending message to one user

--- waMessage.py
+++ waMessage.py
@@ -1,9 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ChromeOptions,Chrome
 import time
 
+
+
+friend = input("chat name: ")
+message = input("message: ")
+
 driver = webdriver.Chrome('/home/vukasin/chromiumDriver/chromedriver')
-driver.get('http://www.google.com')
-search = driver.find_element_by_name('q')
-search.send_keys("hilti")
-search.send_keys(Keys.RETURN)
+driver.get("https://web.whatsapp.com/")
+
+input("enter any key")
+
+#finds the person you want to send the message
+chat = driver.find_element_by_xpath('//span[@title = "{}"]'.format(friend))
+chat.click()
+
+#finds the chat box in whatsapp and types the the text yout entered
+message_box = driver.find_element_by_class_name("_13mgZ")
+message_box.send_keys(message)
+
+#finds the send btn and sends the message to the user
+btn_send = driver.find_element_by_class_name("_3M-N-")
+btn_send.send_keys(Keys.RETURN)
+
+
+
+
+
+
+
+
+ 
